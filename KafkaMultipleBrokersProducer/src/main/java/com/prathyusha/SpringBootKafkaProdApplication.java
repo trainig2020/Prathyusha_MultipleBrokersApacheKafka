24828c8f9b385e0d37982b7e21e14dfd4f93907e
trainig2020 <68064760+trainig2020@users.noreply.github.com>
2021-01-28 21:35:19 +0530

    Add files via upload

---
+++ KafkaMultipleBrokersProducer/src/main/java/com/prathyusha/SpringBootKafkaProdApplication.java
@@ -0,0 +1,13 @@
+package com.prathyusha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootKafkaProdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootKafkaProdApplication.class, args);
+    }
+
+}
